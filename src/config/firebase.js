29ffca9e487bd1248650406374c0412da8b524fb
LuanCalais <luancalais@gmail.com>
2022-11-30 21:17:55 -0300

    create install and config firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,21 @@
+// Conexion configuration file with db firebase
+import firebase from 'firebase'
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLI-NmDHRi9BgBaxsumJrnt4QLoT-dQgQ",
+  authDomain: "eventos-4e296.firebaseapp.com",
+  projectId: "eventos-4e296",
+  storageBucket: "eventos-4e296.appspot.com",
+  messagingSenderId: "185249565687",
+  appId: "1:185249565687:web:7aa19ab5c3cd568e8abcf2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
